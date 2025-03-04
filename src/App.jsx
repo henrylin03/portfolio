8341eea984b14a743d795f5407f5551489123261
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2025-03-04 12:09:58 +1100

    style(global.css): background colour is black by default

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
+import "./styles/global.css";
 
 const App = () => {
   return <MantineProvider></MantineProvider>;
