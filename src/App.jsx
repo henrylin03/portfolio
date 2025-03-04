70a6eeb301bf0785a99f708aa4277c903d548a37
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2025-03-04 11:52:31 +1100

    chore: install mantine core and mantine hooks, and postcss. add postcss config file. add MantineProvider wrapper within App.jsx as recommended.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
-function App() {
-  return <></>;
-}
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+
+const App = () => {
+  return <MantineProvider></MantineProvider>;
+};
 
 export default App;
