7d7a561269c49041e7a112c98061beb5b7da9e28
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2025-03-05 10:54:44 +1100

    style(Header): add .inner class to header. use mantine md variable sizing for header padding.
    the header does not stretch the full width of the page, so adding .inner class was required. this is global at the moment as we want consistent .inner width. media queries may be applied to this in future commits.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,20 +7,22 @@ import styles from "./Header.module.css";
 const Header = () => {
   return (
     <header className={styles.header}>
-      <a href="/">
-        <Group>
-          <img
-            src={logoIcon}
-            className={styles.logoIcon}
-            alt="Henry Lin logo"
-          />
-          <img
-            src={logoText}
-            className={styles.logoText}
-            alt="Henry Lin logo"
-          />
-        </Group>
-      </a>
+      <div className="inner">
+        <a href="/">
+          <Group>
+            <img
+              src={logoIcon}
+              className={styles.logoIcon}
+              alt="Henry Lin logo"
+            />
+            <img
+              src={logoText}
+              className={styles.logoText}
+              alt="Henry Lin logo"
+            />
+          </Group>
+        </a>
+      </div>
     </header>
   );
 };
