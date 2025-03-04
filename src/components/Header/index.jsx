9fd17232780c449e969ac2e6400effeeb992bdcc
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2025-03-04 12:08:00 +1100

    feat(Header): add branding to header. reverse colours of svg for black background

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,28 @@
+import { Box, Group } from "@mantine/core";
+import logoIcon from "/assets/branding/transparent logo - logo only.svg";
+import logoText from "/assets/branding/transparent logo - text only.svg";
+
+import styles from "./Header.module.css";
+
 const Header = () => {
-  return <header>hello</header>;
+  return (
+    <header className={styles.header}>
+      <a href="/">
+        <Group>
+          <img
+            src={logoIcon}
+            className={styles.logoIcon}
+            alt="Henry Lin logo"
+          />
+          <img
+            src={logoText}
+            className={styles.logoText}
+            alt="Henry Lin logo"
+          />
+        </Group>
+      </a>
+    </header>
+  );
 };
 
 export default Header;
