d9219ae259848c723562c27f67ec5f3291c4ced8
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2025-03-05 11:21:03 +1100

    feat(Header): add navigation links

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,37 @@
-import { Box, Group } from "@mantine/core";
+import { Group } from "@mantine/core";
 import logoIcon from "/assets/branding/transparent logo - logo only.svg";
 import logoText from "/assets/branding/transparent logo - text only.svg";
 
 import styles from "./Header.module.css";
 
+const Nav = () => {
+  const LINKS_DATA = [
+    { label: "Services", link: "#services" },
+    { label: "Projects", link: "#projects" },
+    { label: "About", link: "#about" },
+    { label: "Contact", link: "#contact" },
+  ];
+
+  return (
+    <nav className={styles.nav}>
+      {LINKS_DATA.map((l) => (
+        <a
+          href={l.link}
+          className={styles.navLink}
+          aria-label={`Go to ${l.label} section`}
+        >
+          {l.label}
+        </a>
+      ))}
+    </nav>
+  );
+};
+
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className="inner">
+      <div className={styles.inner}>
+        {/* branding */}
         <a href="/">
           <Group>
             <img
@@ -22,6 +46,11 @@ const Header = () => {
             />
           </Group>
         </a>
+
+        {/* nav */}
+        <Nav />
+
+        {/* button(s) */}
       </div>
     </header>
   );
