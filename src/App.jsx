aead2384064ed0d1eb746628b9c2b3bc0ef5c543
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2025-03-05 11:50:31 +1100

    feat(Header): add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { MantineProvider } from "@mantine/core";
+import Header from "./components/Header";
 import "@mantine/core/styles.css";
 import "./styles/global.css";
 
 const App = () => {
-  return <MantineProvider></MantineProvider>;
+  return (
+    <MantineProvider>
+      <Header />
+    </MantineProvider>
+  );
 };
 
 export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,69 @@
+import { Group, Button } from "@mantine/core";
+import logoIcon from "/assets/branding/transparent logo - logo only.svg";
+import logoText from "/assets/branding/transparent logo - text only.svg";
+
+import styles from "./Header.module.css";
+
+const Nav = () => {
+  const LINKS_DATA = [
+    { label: "Services", link: "#services" },
+    { label: "Projects", link: "#projects" },
+    { label: "About", link: "#about" },
+    { label: "Contact", link: "#contact" },
+  ];
+
+  return (
+    <nav className={styles.nav}>
+      {LINKS_DATA.map((l) => (
+        <a
+          href={l.link}
+          className={styles.navLink}
+          aria-label={`Go to ${l.label} section`}
+        >
+          {l.label}
+        </a>
+      ))}
+    </nav>
+  );
+};
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.inner}>
+        {/* branding */}
+        <a href="/" className={styles.branding}>
+          <Group>
+            <img
+              src={logoIcon}
+              className={styles.logoIcon}
+              alt="Henry Lin logo"
+            />
+            <img
+              src={logoText}
+              className={styles.logoText}
+              alt="Henry Lin logo"
+            />
+          </Group>
+        </a>
+
+        {/* nav */}
+        <Nav />
+
+        {/* button(s) */}
+        <Button
+          variant="white"
+          color="black"
+          radius="xl"
+          component="a"
+          href="#contact"
+          className={styles.ctaButton}
+        >
+          Let's chat
+        </Button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
