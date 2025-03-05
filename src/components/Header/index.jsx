141793a69e060eb1db7ed559b3b433f8b61b94e5
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2025-03-05 13:26:52 +1100

    feat: build header for single-page, English-only, portfolio
    includes ensuring header is responsive through using a Burger icon that, on click, opens a drawer for the navigation links and call-to-action button (currently just 'Let's Chat')

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,101 @@
+import { useDisclosure } from "@mantine/hooks";
+import { Group, Button, Burger, Drawer, Stack, Divider } from "@mantine/core";
+import logoIcon from "/assets/branding/transparent logo - logo only.svg";
+import logoText from "/assets/branding/transparent logo - text only.svg";
+import styles from "./Header.module.css";
+
+const LINKS_DATA = [
+  { label: "Services", link: "#services" },
+  { label: "Projects", link: "#projects" },
+  { label: "About", link: "#about" },
+  { label: "Contact", link: "#contact" },
+];
+
+const navLinksArray = LINKS_DATA.map((l) => (
+  <a
+    href={l.link}
+    className={styles.navLink}
+    aria-label={`Go to ${l.label} section`}
+    key={l.label}
+  >
+    {l.label}
+  </a>
+));
+
+const Header = () => {
+  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] =
+    useDisclosure(false);
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.inner}>
+        <a href="/">
+          <Group wrap="nowrap">
+            <img
+              src={logoIcon}
+              className={styles.logoIcon}
+              alt="Henry Lin logo"
+            />
+            <img
+              src={logoText}
+              className={styles.logoText}
+              alt="Henry Lin logo"
+            />
+          </Group>
+        </a>
+
+        <nav className={styles.nav}>
+          <Group visibleFrom="xs">{navLinksArray}</Group>
+        </nav>
+
+        <Button
+          color="white"
+          autoContrast
+          radius="xl"
+          component="a"
+          href="#contact"
+          visibleFrom="xs"
+          className={styles.ctaButton}
+        >
+          Let's chat
+        </Button>
+
+        <Burger
+          color="lightgrey"
+          opened={drawerOpened}
+          onClick={toggleDrawer}
+          size="sm"
+          hiddenFrom="xs"
+          className={styles.burger}
+        />
+      </div>
+
+      {/* drawer */}
+      <Drawer
+        opened={drawerOpened}
+        onClose={closeDrawer}
+        size="sm"
+        padding="md"
+        zIndex={999}
+        hiddenFrom="xs"
+      >
+        <Stack gap="xs">{navLinksArray}</Stack>
+
+        <Divider my="md" />
+
+        <Button
+          color="white"
+          autoContrast
+          radius="xl"
+          component="a"
+          href="#contact"
+          className={styles.ctaButton}
+        >
+          Let's chat
+        </Button>
+      </Drawer>
+    </header>
+  );
+};
+
+export default Header;
