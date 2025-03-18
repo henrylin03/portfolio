2e4340ddc0aa1a74d1dabd9ebaad90d30c183c3a
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2025-03-18 11:02:28 +1100

    feat: add Mantine theme object. use DM Sans font. use 'xl' default border radius for Mantine components.

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { MantineProvider } from "@mantine/core";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 import "@mantine/core/styles.css";
+import theme from "./styles/theme";
 import "./styles/global.css";
 
 const App = () => {
   return (
-    <MantineProvider forceColorScheme="dark">
+    <MantineProvider forceColorScheme="dark" theme={theme}>
       <Header />
 
       <main>
---
+++ src/styles/theme.js
@@ -0,0 +1,8 @@
+import { createTheme } from "@mantine/core";
+
+const theme = createTheme({
+  fontFamily: "DM Sans, sans-serif",
+  defaultRadius: "xl",
+});
+
+export default theme;
