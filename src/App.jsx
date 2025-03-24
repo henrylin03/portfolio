c1b4aeeecdf6e2f555ffc6cfd220c841120e8487
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2025-03-24 15:44:16 +1100

    feat: build experience section.
    experience section contains all the jobs i've worked at before. added an 'experiences.js' data file to make it easier to update the jobs i've worked at before and have them dynamically generate <article> elements.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 import ServicesSection from "./components/ServicesSection";
 import ProjectsSection from "./components/ProjectsSection";
+import ExperienceSection from "./components/ExperienceSection";
 import "@mantine/core/styles.css";
 import theme from "./styles/theme";
 import "./styles/global.css";
@@ -16,6 +17,7 @@ const App = () => {
         <HeroSection />
         <ServicesSection />
         <ProjectsSection />
+        <ExperienceSection />
       </main>
 
       <footer></footer>
