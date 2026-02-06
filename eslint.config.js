927aab851f9de0fd2aeb8f7b199b46567ed66e83
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-02-06 19:05:14 +1100

    chore(esling.config): add further rules and recommend configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,15 @@
 import eslintPluginAstro from "eslint-plugin-astro";
+import js from "@eslint/js";
+
 export default [
-  // add more generic rule sets here, such as:
-  // js.configs.recommended,
+  js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
+
   {
     rules: {
-      // override/add rules settings here, such as:
-      // "astro/no-set-html-directive": "error"
+      "astro/no-set-html-directive": "error",
+      "astro/no-unused-css-selector": "warn",
+      "astro/prefer-class-list-directive": "warn",
     },
   },
 ];
