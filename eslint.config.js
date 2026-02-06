785f280b196ebd02653c5be67710d84a58ee5b65
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-02-06 19:06:39 +1100

    chore(eslint): add linting for accessibility elements

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import js from "@eslint/js";
 export default [
   js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
+  ...eslintPluginAstro.configs["jsx-a11y-recommended"],
 
   {
     rules: {
