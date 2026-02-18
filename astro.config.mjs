a747974a5f8e67231d6184f2d928b6c0f364fffe
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-02-18 13:16:22 +1100

    feat: generate sitemap using astro-sitemap plugin, including configuring the last modified date. add robots.txt that references sitemap, and add reference to sitemap in the head component

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,18 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  site: "https://henrylin.io",
+  integrations: [
+    sitemap({
+      changefreq: "monthly",
+      lastmod: new Date("2026-02-18"),
+    }),
+  ],
 });
